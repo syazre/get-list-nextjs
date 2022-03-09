3a6055de7b5186769107755d16c3cd5497d33844
Syafiq Syazre <syazre.dev@gmail.com>
2022-03-09 15:08:37 +0800

    Remove header in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,3 @@
 module.exports = {
-    async headers() {
-      return [
-        {
-          source: '/',
-          headers: [
-            {
-            "access-control-allow-origin": "*",
-            "content-type": "text/plain",
-            "vary": "Accept-Encoding",
-            }
-          ],
-        },
-      ]
-    },
+    
   }
