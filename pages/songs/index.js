9ddaf6a9bb9c2403b204f1ee183e4a016f82083b
Syafiq Syazre <syazre.dev@gmail.com>
2022-06-23 10:05:51 +0800

    Sample useState and useEffect

--- pages/songs/index.js
+++ pages/songs/index.js
@@ -1,30 +1,42 @@
-// import { jobs } from '../../data';
 
-// const Jobs = ({ jobList }) => {
-//   return (
+import React, { useState, useEffect } from 'react';
 
-//     <table className="jobs">
-//       <tr>
-//       <th>Title</th> <th>Company</th> <th>Location</th>
-//       </tr>
-//       {jobList.map((job) => (
-//         <tr key={job.id}>
-//           <td>{job.title}</td>
-//           <td>{job.company}</td>
-//           <td>{job.location}</td>
-//         </tr>
-//       ))}
-//     </table>
-//   )
-// }
+const IntervalExample = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [posts, setPosts]=useState([])
 
-// export const getStaticProps = async () => {
-//   return {
-    
-//     props: {
-//       jobList: jobs
-//     }
-//   }
-// }
+    async function getPosts (id) {
+        const url = 'https://jsonplaceholder.typicode.com/posts/' + id;
+        const res = await fetch(url)
+        //const res = await fetch('https://jsonplaceholder.typicode.com/posts/')
+        
+        const json = await res.json()
+        setPosts(json)
+    }
 
-// export default Jobs;
+  useEffect(() => {
+    var seconds = 1;
+    const interval = setInterval(() => {
+      setSeconds(seconds => seconds + 1);
+      getPosts(seconds);
+      seconds++;
+
+      if(seconds === 20){
+            clearInterval(interval);
+        }
+    }, 1000);
+    getPosts(1);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {seconds} seconds have elapsed since mounting. {' '}
+        {posts.title}
+      </header>
+    </div>
+  );
+};
+
+export default IntervalExample;
